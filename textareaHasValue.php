e4eb44d76279293c3a204875cefa85f56aca1612
Adam Patterson <awpatterson217@gmail.com>
2016-11-01 13:36:43 -0500

    Uploading php file

---
+++ textareaHasValue.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+	<title>
+		textareaHasValue
+	</title>
+</head>
+<body>
+	<?php
+		// Is the user editing a post, or creating a new one.
+		$isEditing = true;
+	?>
+
+
+
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+	<?php
+	/*
+	*@author Adam Patterson
+	*/
+	if($isEditing){
+		echo "<script>
+		$(document).ready(function(){
+			
+			$('#editDoc').css('display', 'table-row');
+			//$('#the_submit').prop('disabled','false');
+			//$('#the_submit').attr('title','Good To Go'); 
+			$('#the_submit').prop('disabled','true');
+			$('#the_submit').attr('title','Edit Must Be Documented');
+
+		});
+
+		$(document).ready(function () {
+			$('#editTextArea').keyup(function(){
+
+				var val = $.trim($(\"#editTextArea\").val());
+				if (val != \"\") {
+				$('#the_submit').removeAttr('disabled');
+				$('#the_submit').attr('title','Good To Go');
+				//$('#the_submit').prop('disabled','true');
+				//$('#the_submit').attr('title','Edit Must Be Documented'); 
+				}
+			});
+		});
+
+		$(document).ready(function () {
+			$('#editTextArea').keyup(function(){
+
+				var val = $.trim($(\"#editTextArea\").val());
+				if (val == \"\") {
+				//$('#the_submit').removeAttr('disabled');
+				//$('#the_submit').attr('title','Good To Go');
+				$('#the_submit').prop('disabled','true');
+				$('#the_submit').attr('title','Edit Must Be Documented'); 
+				}
+			});
+		});
+		</script>";
+	}
+	?>
+	<table>
+	<tr style='display:none' id="editDoc">
+	<td>
+	<div>    
+	<h1>documentation of edit</h1><br />
+	What have you changed?<br />
+	<textarea id="editTextArea"></textarea>
+	</div>
+	</td>
+	</tr>
+	</table>
+	
+	<input type="submit" id="the_submit">
+	
+</body>
+</html>
